dcccc058f57a6478998d7d40f529f7de3bd7e880
David Kuri <david.kuri@st.ovgu.de>
2014-01-14 22:45:48 +0100

    Some notes for myself

--- ScarletGamma/src/actions/Attack.cpp
+++ ScarletGamma/src/actions/Attack.cpp
@@ -12,5 +12,19 @@ Attack::Attack()
 
 void Attack::Execute()
 {
-    // TODO
+    // This functions is called when a player clicks "Attack" on an object
+    // What should happen next?
+
+    // 1)  Check if the player is in proximity to the enemy or has a ranged weapon
+    // 2)  Show a dialog to the player, asking for the Hit Roll values
+    // 3)  Send the entered values to the Game Master
+    //     Server side is handled elsewhere I hope?!
+    //
+    //     Server replies:
+    // 4a) MISSED, were done. Kill the action!
+    // 4b) CRIT CHANCE, roll again and send it to the server
+    // 4c) HIT, enter damage (remember wether we did critical damage to show
+    //     message "damage is auto-doubled") and send it to the server
+    //
+    // done!
 }

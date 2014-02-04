6aadc93e3dd58c093a52a2e173524b26f5f89bb5
David Kuri <david.kuri@st.ovgu.de>
2014-02-04 21:58:02 +0100

    Implemented the WalkTo action. We could start to give priorities to actions and use left-click for "execute highest-priority possible action".

--- ScarletGamma/src/actions/WalkTo.cpp
+++ ScarletGamma/src/actions/WalkTo.cpp
@@ -1,7 +1,12 @@
 #include "WalkTo.hpp"
 #include "Constants.hpp"
-#include "../core/Object.hpp"
+#include "core/Object.hpp"
 #include "network/ActionMessages.hpp"
+#include "Game.hpp"
+#include "core/World.hpp"
+#include "core/PredefinedProperties.hpp"
+#include "core/Map.hpp"
+#include "actions/ActionPool.hpp"
 
 using namespace Actions;
 
@@ -14,14 +19,36 @@ WalkTo::WalkTo() : Action(STR_ACT_WALKTO, ActionType::MOVE_ACTION)
 
 void WalkTo::Execute()
 {
-    //TODO: Actual do something
+    Core::Object* executor = g_Game->GetWorld()->GetObject(m_executor);
+    assert(executor);
+
+    if (!executor->HasProperty(STR_PROP_TARGET))
+        executor->Add(Core::PROPERTY::TARGET);
+
+    if (!executor->HasProperty(STR_PROP_PATH))
+        executor->Add(Core::PROPERTY::PATH);
+
+    // Clear the target
+    executor->GetProperty(STR_PROP_TARGET).SetValue(STR_EMPTY);
+    Core::Property& path = executor->GetProperty(STR_PROP_PATH);
+    path.ClearObjects();
+    path.SetValue(STR_FALSE);
+    executor->AppendToPath( m_target );
+
+    // Reevaluate object to get it updated
+    g_Game->GetWorld()->GetMap(executor->GetParentMap())->ReevaluateActiveObject(m_executor);
+
+    // Action is done
+    Actions::ActionPool::Instance().EndLocalAction();
 }
 
 Action* WalkTo::Clone(Core::ObjectID _executor, Core::ObjectID _target)
 {
     WalkTo* result = new WalkTo();
 
-    // TODO: copy all values
+    result->m_executor = _executor; // set executor
+    result->m_target = _target; // set target
+    result->m_id = m_id; // copy ID
 
     return dynamic_cast<Action*>(result);
 }

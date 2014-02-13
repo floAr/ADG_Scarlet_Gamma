381fb97c7c9a1b2bddc5742532b7a75a2bd82e03
Johannes Jendersie <jojendersie@web.de>
2014-02-13 19:25:29 +0100

    bugfix: pulling a new player to the map did not reset his target

--- ScarletGamma/src/states/MasterState.cpp
+++ ScarletGamma/src/states/MasterState.cpp
@@ -275,13 +275,13 @@ namespace States {
 					} else if( m_draggedContent->from == Interfaces::DragContent::PLAYERS_LIST )
 					{
 						// Insert original to the map (player layer)
-						if( !m_draggedContent->object->IsLocatedOnAMap() )
-							GetCurrentMap()->Add( m_draggedContent->object->ID(), x, y, 5 );
+						Object* object = const_cast<Object*>(m_draggedContent->object);
+						if( !object->IsLocatedOnAMap() )
+							GetCurrentMap()->Add( object->ID(), x, y, 5 );
 						else {
-							Object* object = const_cast<Object*>(m_draggedContent->object);
 							GetCurrentMap()->SetObjectPosition( object, tilePos );
-							object->ResetTarget();
 						}
+						object->ResetTarget();
 					}
 				}
 			}
